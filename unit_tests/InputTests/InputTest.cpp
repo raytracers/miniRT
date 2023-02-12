b6f08dd5ff4741bcb983d6da8ad7a43f55287dc4
lfarias- (Leonardo Farias) <leofariasrj25@gmail.com>
2023-02-12 20:03:06 -0300

    Refactor the tests to conform to UpperCamelCase

--- unit_tests/InputTests/InputTest.cpp
+++ unit_tests/InputTests/InputTest.cpp
@@ -5,14 +5,14 @@ extern "C"
 	#include "../../headers/mini_rt.h"
 }
 
-TEST(InputTests, argcTest)
+TEST(InputTests, ArgcTest)
 {
 	EXPECT_EQ(check_argc(1), 0);
 	EXPECT_EQ(check_argc(0), 1);
 	EXPECT_EQ(check_argc(2), 1);
 }
 
-TEST(InputTests, filenameTest)
+TEST(InputTests, FilenameTest)
 {
 	EXPECT_EQ(check_file_extension((char *) "scene.rt"), 0);
 	EXPECT_EQ(check_file_extension((char *) ".rt"), 1);
