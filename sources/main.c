28f35090499e4c73acc00ce8da2e876ba451cdd2
Leonardo Farias <leofariasrj25@gmail.com>
2023-02-13 18:13:56 -0300

    5 criação da mainc (#6)
    * initial arg validation check on main.c
    
    * Add file reading initial function
    
    * style: change scene_reader to scene_loader for better abstraction

---
+++ sources/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/12 20:56:33 by lfarias-          #+#    #+#             */
+/*   Updated: 2023/02/13 03:51:32 by lfarias-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/mini_rt.h"
+
+int	main(int argc, char **argv)
+{
+	int	scene_fd;
+
+	if (validate_args(argc, argv) != 0)
+		return (1);
+	scene_fd = scene_open(argv[1]);		
+	return (0);	
+}
