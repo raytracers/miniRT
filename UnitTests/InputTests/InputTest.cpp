5bfbc5ae65d5264e94b519aa269c259b1ae6da86
lfarias- (Leonardo Farias) <leofariasrj25@gmail.com>
2023-02-14 23:18:47 -0300

    test: Add more edge cases to filenameTest

--- UnitTests/InputTests/InputTest.cpp
+++ UnitTests/InputTests/InputTest.cpp
@@ -12,11 +12,28 @@ TEST(InputTests, ArgcTest)
 	EXPECT_EQ(check_argc(2), 1);
 }
 
-TEST(InputTests, FilenameTest)
+TEST(InputTests, filenameTest)
 {
-	EXPECT_EQ(check_file_extension((char *) "scene.rt"), 0);
-	EXPECT_EQ(check_file_extension((char *) ".rt"), 1);
-	EXPECT_EQ(check_file_extension((char *) ""), 1);
-	EXPECT_EQ(check_file_extension((char *) "rt"), 1);
-	EXPECT_EQ(check_file_extension((char *) "scene"), 1);
+    EXPECT_EQ(check_file_extension((char *) "scene.rt"), 0);
+    EXPECT_EQ(check_file_extension((char *) "o.rt"), 0);
+    EXPECT_EQ(check_file_extension((char *) "..rt"), 0);
+    EXPECT_EQ(check_file_extension((char *) "rt.rt"), 0);
+    EXPECT_EQ(check_file_extension((char *) "scene.rt.rt"), 0);
+    EXPECT_EQ(check_file_extension((char *) "     .rt"), 0);
+    EXPECT_EQ(check_file_extension((char *) ".rt "), 1);
+    EXPECT_EQ(check_file_extension((char *) ".rt"), 1);
+    EXPECT_EQ(check_file_extension((char *) "-rt"), 1);
+    EXPECT_EQ(check_file_extension((char *) ";rt"), 1);
+    EXPECT_EQ(check_file_extension((char *) ":rt"), 1);
+    EXPECT_EQ(check_file_extension((char *) ".rtrt"), 1);
+    EXPECT_EQ(check_file_extension((char *) ""), 1);
+    EXPECT_EQ(check_file_extension((char *) "       "), 1);
+    EXPECT_EQ(check_file_extension((char *) "\'       \'"), 1);
+    EXPECT_EQ(check_file_extension((char *) "rt"), 1);
+    EXPECT_EQ(check_file_extension((char *) "scene"), 1);
+    EXPECT_EQ(check_file_extension((char *) "\'.rt\'"), 1);
+    EXPECT_EQ(check_file_extension((char *) "\'scene.rt\'"), 1);
+    EXPECT_EQ(check_file_extension((char *) "scene.rta"), 1);
+    EXPECT_EQ(check_file_extension((char *) "scenert"), 1);
+    EXPECT_EQ(check_file_extension((char *) "scene.tr"), 1);
 }
