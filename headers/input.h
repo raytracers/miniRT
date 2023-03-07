e8120355cbec3bb161fc4134cbb3b61abc82857e
Gabriel Correia lima cardoso <gcorreia@c1t3s5.42.rio>
2023-03-07 16:13:02 -0300

    Refactor: reorganized headers

--- headers/input.h
+++ headers/input.h
@@ -1,22 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   scene.h                                            :+:      :+:    :+:   */
+/*   input.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcorreia <gcorreia@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 12:20:54 by gcorreia          #+#    #+#             */
-/*   Updated: 2023/03/07 12:42:06 by gcorreia         ###   ########.fr       */
+/*   Updated: 2023/03/07 16:11:41 by gcorreia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCENE_H
-# define SCENE_H
+#ifndef INPUT_H
+# define INPUT_H
 
 # include "types.h"
 
+/* *********************** VALIDATION FUNCTIONS ***************************** */
+
+int				check_argc(int argc);
+int				check_file_extension(char *filename);
+int				validate_args(int argc, char **argv);
+
+/* ************************************************************************** */
+
+/* ********************* INTERPRETATION FUNCTIONS *************************** */
+
+int				scene_open(char *filename);
 enum e_element	get_element(char *elem);
 int				get_color(char *rgb);
-int				scene_open(char *filename);
+
+/* ************************************************************************** */
 
 #endif
--- headers/mini_rt.h
+++ headers/mini_rt.h
@@ -6,7 +6,7 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 15:26:53 by lfarias-          #+#    #+#             */
-/*   Updated: 2023/03/07 12:23:32 by gcorreia         ###   ########.fr       */
+/*   Updated: 2023/03/07 16:12:15 by gcorreia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,7 @@
 # include "mlx.h"
 # include "mlx_functions.h"
 # include "types.h"
-# include "scene.h"
+# include "input.h"
 # include <stdlib.h>
-
-/* *********************** VALIDATION FUNCTIONS ***************************** */
-
-int				check_argc(int argc);
-int				check_file_extension(char *filename);
-int				validate_args(int argc, char **argv);
-
-/* ************************************************************************** */
 
 #endif
