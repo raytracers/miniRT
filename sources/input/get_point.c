7b91f3cc87a064c270b3065ee99a49140a1e5886
Gabriel Correia lima cardoso <gcorreia@c1t3s4.42.rio>
2023-03-08 15:44:38 -0300

    Refactor: refactored the code inside the get_point function

--- sources/input/get_point.c
+++ sources/input/get_point.c
@@ -6,38 +6,38 @@
 /*   By: gcorreia <gcorreia@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 14:46:58 by gcorreia          #+#    #+#             */
-/*   Updated: 2023/03/08 15:26:49 by gcorreia         ###   ########.fr       */
+/*   Updated: 2023/03/08 15:40:34 by gcorreia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/mini_rt.h"
 
+static int	strs_are_valid(char** strs);
+
 int	get_point(char *str, t_point *point)
 {
 	char	**pts;
-	int		i;
 
 	pts = ft_split(str, ',');
-	if (!pts)
-		return (1);
-	if (ft_arraylen(pts) != 3)
+	if (!pts || ft_arraylen(pts) != 3 || !strs_are_valid(pts))
 	{
 		ft_free_array(pts);
 		return (1);
-	}
-	i = 0;
-	while (pts[i])
-	{
-		if (!ft_isdouble_str(pts[i]))
-		{
-			ft_free_array(pts);
-			return (1);
-		}
-		++i;
 	}
 	point->x = ft_atof(pts[0]);
 	point->y = ft_atof(pts[1]);
 	point->z = ft_atof(pts[2]);
 	ft_free_array(pts);
 	return (0);
+}
+
+static int	strs_are_valid(char** strs)
+{
+	while (*strs)
+	{
+		if (!ft_isdouble_str(*strs))
+			return (0);
+		++strs;
+	}
+	return (1);
 }
