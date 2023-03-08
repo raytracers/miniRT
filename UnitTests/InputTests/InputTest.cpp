cc3a484de9f63df16107b5a5f19b0af32e174ae5
lfarias- (Leonardo Farias) <leofariasrj25@gmail.com>
2023-03-07 21:00:53 -0300

    mend
    Fix InputTest to reflect changes on arg_validator and add more edge cases

--- UnitTests/InputTests/InputTest.cpp
+++ UnitTests/InputTests/InputTest.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 
+#define TRUE 1
+#define FALSE 0
+
 extern "C"
 {
 	#include "../../headers/mini_rt.h"
@@ -7,16 +10,41 @@ extern "C"
 
 TEST(InputTests, ArgcTest)
 {
-	EXPECT_EQ(check_argc(1), 0);
-	EXPECT_EQ(check_argc(0), 1);
-	EXPECT_EQ(check_argc(2), 1);
+	EXPECT_EQ(check_argc(1), TRUE);
+	EXPECT_EQ(check_argc(0), FALSE);
+	EXPECT_EQ(check_argc(2), FALSE);
+	EXPECT_EQ(check_argc(-1), FALSE);
+	EXPECT_EQ(check_argc(-2), FALSE);
+	EXPECT_EQ(check_argc(-99), FALSE);
 }
 
 TEST(InputTests, FilenameTest)
 {
-	EXPECT_EQ(check_file_extension((char *) "scene.rt"), 0);
-	EXPECT_EQ(check_file_extension((char *) ".rt"), 1);
-	EXPECT_EQ(check_file_extension((char *) ""), 1);
-	EXPECT_EQ(check_file_extension((char *) "rt"), 1);
-	EXPECT_EQ(check_file_extension((char *) "scene"), 1);
+	// The expected output
+	EXPECT_EQ(check_file_extension((char *) "scene.rt"), TRUE);
+	EXPECT_EQ(check_file_extension((char *) " .rt"), TRUE);
+	EXPECT_EQ(check_file_extension((char *) "-.rt"), TRUE);
+	EXPECT_EQ(check_file_extension((char *) "a.rt"), TRUE);
+	EXPECT_EQ(check_file_extension((char *) "42.rt"), TRUE);
+	EXPECT_EQ(check_file_extension((char *) "       .rt"), TRUE);
+	EXPECT_EQ(check_file_extension((char *) ".rt.rt"), TRUE);
+
+	// edge cases
+	EXPECT_EQ(check_file_extension((char *) "scene.RT"), FALSE);
+	EXPECT_EQ(check_file_extension((char *) "scene.Rt"), FALSE);
+	EXPECT_EQ(check_file_extension((char *) "scene.rT"), FALSE);
+	EXPECT_EQ(check_file_extension((char *) ".rt"), FALSE);
+	EXPECT_EQ(check_file_extension((char *) ""), FALSE);
+	EXPECT_EQ(check_file_extension((char *) "rt"), FALSE);
+	EXPECT_EQ(check_file_extension((char *) "scene"), FALSE);
+	EXPECT_EQ(check_file_extension((char *) ".rt"), FALSE);
+
+	// testing for chars after the extension 
+	EXPECT_EQ(check_file_extension((char *) ".rt "), FALSE);
+	EXPECT_EQ(check_file_extension((char *) ".rt      "), FALSE);
+	EXPECT_EQ(check_file_extension((char *) ".rta"), FALSE);
+	EXPECT_EQ(check_file_extension((char *) ".rt-"), FALSE);
+	EXPECT_EQ(check_file_extension((char *) ".rt0"), FALSE);
+	EXPECT_EQ(check_file_extension((char *) ".rt!"), FALSE);
+	EXPECT_EQ(check_file_extension((char *) ".rt\0"), FALSE);
 }
