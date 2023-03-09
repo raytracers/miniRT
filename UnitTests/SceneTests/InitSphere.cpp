ab4a0fc7719b3aae1a8b159b56e041d03bda7865
lfarias- <lfarias-@student.42.rio>
2023-03-09 14:00:22 -0300

    Refactor: Change init_sphere to ue ft_arraylen and add two more tests

--- UnitTests/SceneTests/InitSphere.cpp
+++ UnitTests/SceneTests/InitSphere.cpp
@@ -17,6 +17,27 @@ TEST(SceneTests, InitSphereTest)
 	EXPECT_DOUBLE_EQ(element.sphere.origin.z, 20.6);
 	EXPECT_DOUBLE_EQ(element.sphere.diameter, 12.6);
 	EXPECT_EQ(element.sphere.color, 0x0A00FF);
+	ft_free_array(attributes);
 
+	sphere_str = (char *) "-1.0,-1.0,-1.0 -42.42 255,255,255";
+	attributes = ft_split(sphere_str, ' ');
+	op_result = init_sphere(attributes, &element);
+	EXPECT_EQ(op_result, 0);
+	EXPECT_DOUBLE_EQ(element.sphere.origin.y, -1.0);
+	EXPECT_DOUBLE_EQ(element.sphere.origin.x, -1.0);
+	EXPECT_DOUBLE_EQ(element.sphere.origin.z, -1.0);
+	EXPECT_DOUBLE_EQ(element.sphere.diameter, -42.42);
+	EXPECT_EQ(element.sphere.color, 0xFFFFFF);
+	ft_free_array(attributes);
+
+	sphere_str = (char *) "0.0,0.0,0.0 0.0 0,0,0";
+	attributes = ft_split(sphere_str, ' ');
+	op_result = init_sphere(attributes, &element);
+	EXPECT_EQ(op_result, 0);
+	EXPECT_DOUBLE_EQ(element.sphere.origin.y, 0.0);
+	EXPECT_DOUBLE_EQ(element.sphere.origin.x, 0.0);
+	EXPECT_DOUBLE_EQ(element.sphere.origin.z, 0.0);
+	EXPECT_DOUBLE_EQ(element.sphere.diameter, 0.0);
+	EXPECT_EQ(element.sphere.color, 0x000000);
 	ft_free_array(attributes);
 }
--- sources/input/init_sphere.c
+++ sources/input/init_sphere.c
@@ -6,7 +6,7 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 15:57:10 by lfarias-          #+#    #+#             */
-/*   Updated: 2023/03/09 13:36:02 by lfarias-         ###   ########.fr       */
+/*   Updated: 2023/03/09 13:58:26 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,9 @@ int	init_sphere(char **attributes, union u_element *element)
 	int			color;
 	t_sphere	*sphere;
 
-	attr_len = 0;
 	color = 0;
 	sphere = &element->sphere;
-	while (attributes[attr_len])
-		attr_len++;
+	attr_len = ft_arraylen(attributes);
 	if (attr_len != 3)
 		return (1);
 	if (get_point(attributes[0], &sphere->origin))
