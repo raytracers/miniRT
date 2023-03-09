83a9f866eee18befb588d6ba61f069eb9f698911
lfarias- <lfarias-@student.42.rio>
2023-03-09 15:56:38 -0300

    chore: init_cylinder prototype and intial test

--- headers/input.h
+++ headers/input.h
@@ -6,7 +6,7 @@
 /*   By: gcorreia <gcorreia@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 12:20:54 by gcorreia          #+#    #+#             */
-/*   Updated: 2023/03/09 13:30:17 by lfarias-         ###   ########.fr       */
+/*   Updated: 2023/03/09 15:53:52 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ enum e_element	get_element(char *elem);
 int				get_color(char *rgb);
 int				get_point(char *str, t_point *point);
 int				init_sphere(char **attributes, union u_element *sphere);
+int				init_cylinder(char **attributes, union u_element *cylinder);
 int				is_normalized(t_point vector);
 
 /* ************************************************************************** */
