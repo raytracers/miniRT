383991cd05919248382c858f46dc1fd2fc2fd10a
lfarias- <lfarias-@student.42.rio>
2023-03-09 17:53:40 -0300

    Test: Add basic functionality tests of InitCylinder.cpp

--- UnitTests/SceneTests/InitCylinder.cpp
+++ UnitTests/SceneTests/InitCylinder.cpp
@@ -11,40 +11,48 @@ TEST(SceneTests, ValidCylinderTest)
 	char *cylinder_str = (char *) "50.0,0.0,20.6 0.0,0.0,1.0 14.2 21.42 10,0,255";
 	char **attributes = ft_split(cylinder_str, ' ');
 	union u_element element;
-	int op_result = init_sphere(attributes, &element);
+	int op_result = init_cylinder(attributes, &element);
 	EXPECT_EQ(op_result, 0);
 	EXPECT_DOUBLE_EQ(element.cylinder.origin.x, 50.0);
 	EXPECT_DOUBLE_EQ(element.cylinder.origin.y, 0.0);
 	EXPECT_DOUBLE_EQ(element.cylinder.origin.z, 20.6);
-	EXPECT_DOUBLE_EQ(element.cylinder.orientation.x, 50.0);
+	EXPECT_DOUBLE_EQ(element.cylinder.orientation.x, 0.0);
 	EXPECT_DOUBLE_EQ(element.cylinder.orientation.y, 0.0);
-	EXPECT_DOUBLE_EQ(element.cylinder.orientation.z, 20.6);
+	EXPECT_DOUBLE_EQ(element.cylinder.orientation.z, 1.0);
 	EXPECT_DOUBLE_EQ(element.cylinder.diameter, 14.2);
 	EXPECT_DOUBLE_EQ(element.cylinder.height, 21.42);
-	EXPECT_EQ(element.sphere.color, 0x0A00FF);
+	EXPECT_EQ(element.cylinder.color, 0x0A00FF);
 	ft_free_array(attributes);
 
-	/*sphere_str = (char *) "-1.0,-1.0,-1.0 -42.42 255,255,255";
-	attributes = ft_split(sphere_str, ' ');
-	op_result = init_sphere(attributes, &element);
+	cylinder_str = (char *) "0.0,0.0,0.0 1.0,0.0,0.0 0.0 0.0 0,0,0";
+	attributes = ft_split(cylinder_str, ' ');
+	op_result = init_cylinder(attributes, &element);
 	EXPECT_EQ(op_result, 0);
-	EXPECT_DOUBLE_EQ(element.sphere.origin.y, -1.0);
-	EXPECT_DOUBLE_EQ(element.sphere.origin.x, -1.0);
-	EXPECT_DOUBLE_EQ(element.sphere.origin.z, -1.0);
-	EXPECT_DOUBLE_EQ(element.sphere.diameter, -42.42);
-	EXPECT_EQ(element.sphere.color, 0xFFFFFF);
+	EXPECT_DOUBLE_EQ(element.cylinder.origin.x, 0.0);
+	EXPECT_DOUBLE_EQ(element.cylinder.origin.y, 0.0);
+	EXPECT_DOUBLE_EQ(element.cylinder.origin.z, 0.0);
+	EXPECT_DOUBLE_EQ(element.cylinder.orientation.x, 1.0);
+	EXPECT_DOUBLE_EQ(element.cylinder.orientation.y, 0.0);
+	EXPECT_DOUBLE_EQ(element.cylinder.orientation.z, 0.0);
+	EXPECT_DOUBLE_EQ(element.cylinder.diameter, 0.0);
+	EXPECT_DOUBLE_EQ(element.cylinder.height, 0.0);
+	EXPECT_EQ(element.cylinder.color, 0x000000);
 	ft_free_array(attributes);
 
-	sphere_str = (char *) "0.0,0.0,0.0 0.0 0,0,0";
-	attributes = ft_split(sphere_str, ' ');
-	op_result = init_sphere(attributes, &element);
+	cylinder_str = (char *) "-1.0,-1.0,-1.0 -1.0,-1.0,-1.0 -1.0 -1.0 0,0,0";
+	attributes = ft_split(cylinder_str, ' ');
+	op_result = init_cylinder(attributes, &element);
 	EXPECT_EQ(op_result, 0);
-	EXPECT_DOUBLE_EQ(element.sphere.origin.y, 0.0);
-	EXPECT_DOUBLE_EQ(element.sphere.origin.x, 0.0);
-	EXPECT_DOUBLE_EQ(element.sphere.origin.z, 0.0);
-	EXPECT_DOUBLE_EQ(element.sphere.diameter, 0.0);
-	EXPECT_EQ(element.sphere.color, 0x000000);
-	ft_free_array(attributes);*/
+	EXPECT_DOUBLE_EQ(element.cylinder.origin.x, -1.0);
+	EXPECT_DOUBLE_EQ(element.cylinder.origin.y, -1.0);
+	EXPECT_DOUBLE_EQ(element.cylinder.origin.z, -1.0);
+	EXPECT_DOUBLE_EQ(element.cylinder.orientation.x, -1.0);
+	EXPECT_DOUBLE_EQ(element.cylinder.orientation.y, -1.0);
+	EXPECT_DOUBLE_EQ(element.cylinder.orientation.z, -1.0);
+	EXPECT_DOUBLE_EQ(element.cylinder.diameter, -1.0);
+	EXPECT_DOUBLE_EQ(element.cylinder.height, -1.0);
+	EXPECT_EQ(element.cylinder.color, 0x000000);
+	ft_free_array(attributes);
 }
 
 /*void	test_init_cylinder(char *input_str)
--- sources/input/init_cylinder.c
+++ sources/input/init_cylinder.c
@@ -6,16 +6,37 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 15:32:20 by lfarias-          #+#    #+#             */
-/*   Updated: 2023/03/09 15:38:30 by lfarias-         ###   ########.fr       */
+/*   Updated: 2023/03/09 17:53:03 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/mini_rt.h"
+#include <stdio.h>
 
 int	init_cylinder(char **attributes, union u_element *element)
 {
 	// 50.0,0.0,20.6 0.0,0.0,1.0 14.2 21.42 10,0,255
-	(void)attributes;
-	(void)element;
-	return (1);
+	int			attr_len;
+	int			color;
+	t_cylinder	*cylinder;
+
+	color = 0;
+	attr_len = ft_arraylen(attributes);
+	cylinder = &element->cylinder;
+	if (attr_len != 5)
+		return (1);
+	if (get_point(attributes[0], &cylinder->origin))
+		return (1);
+	if (get_point(attributes[1], &cylinder->orientation) && \
+		is_normalized(cylinder->orientation))
+		return (1);
+	if (!ft_isdouble_str(attributes[2]) || !ft_isdouble_str(attributes[3]))
+		return (1);
+	cylinder->diameter = ft_atof(attributes[2]);
+	cylinder->height = ft_atof(attributes[3]);
+	color = get_color(attributes[4]);
+	if (color == -1)
+		return (1);
+	cylinder->color = color;
+	return (0);
 }
