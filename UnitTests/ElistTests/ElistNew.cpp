4c244bd0585a8c848a3653e1cdf79afcf293f743
GabrielCLC <gabrielclcardoso@gmail.com>
2023-03-09 19:18:09 -0300

    Tests: Added tests for elist new with cameras and cylinders

--- UnitTests/ElistTests/ElistNew.cpp
+++ UnitTests/ElistTests/ElistNew.cpp
@@ -11,6 +11,7 @@ TEST(ElistNew, NewSphere)
 	char **content = ft_split("0.0,0.0,20.6 12.6 10,0,255", ' ');
 
 	node = elist_new(sphere, content);
+	ASSERT_NE(node, nullptr);
 	EXPECT_DOUBLE_EQ(node->element.sphere.origin.y, 0.0);
 	EXPECT_DOUBLE_EQ(node->element.sphere.origin.x, 0.0);
 	EXPECT_DOUBLE_EQ(node->element.sphere.origin.z, 20.6);
@@ -22,6 +23,60 @@ TEST(ElistNew, NewSphere)
 	
 	content = ft_split("0.0,0.0,20.6 10,0,255", ' ');
 	node = elist_new(sphere, content);
+	EXPECT_EQ(node, nullptr);
+
+	free(node);
+	ft_free_array(content);
+}
+
+TEST(ElistNew, NewCamera)
+{
+	t_elist	*node;
+	char **content = ft_split("-50,0,20 0,0,1 70", ' ');
+
+	node = elist_new(camera, content);
+	ASSERT_NE(node, nullptr);
+	EXPECT_DOUBLE_EQ(node->element.camera.origin.x, -50.0);
+	EXPECT_DOUBLE_EQ(node->element.camera.origin.y, 0.0);
+	EXPECT_DOUBLE_EQ(node->element.camera.origin.z, 20.0);
+	EXPECT_DOUBLE_EQ(node->element.camera.orientation.x, 0.0);
+	EXPECT_DOUBLE_EQ(node->element.camera.orientation.y, 0.0);
+	EXPECT_DOUBLE_EQ(node->element.camera.orientation.z, 1.0);
+	EXPECT_EQ(node->element.camera.fov, 70);
+
+	free(node);
+	ft_free_array(content);
+	
+	content = ft_split("-50,0,20 ,, 70", ' ');
+	node = elist_new(camera, content);
+	EXPECT_EQ(node, nullptr);
+
+	free(node);
+	ft_free_array(content);
+}
+
+TEST(ElistNew, NewCylinder)
+{
+	t_elist	*node;
+	char **content = ft_split("50.0,0.0,20.6 0.0,0.0,1.0 14.2 21.42 10,0,255", ' ');
+
+	node = elist_new(cylinder, content);
+	ASSERT_NE(node, nullptr);
+	EXPECT_DOUBLE_EQ(node->element.cylinder.origin.x, 50.0);
+	EXPECT_DOUBLE_EQ(node->element.cylinder.origin.y, 0.0);
+	EXPECT_DOUBLE_EQ(node->element.cylinder.origin.z, 20.6);
+	EXPECT_DOUBLE_EQ(node->element.cylinder.orientation.x, 0.0);
+	EXPECT_DOUBLE_EQ(node->element.cylinder.orientation.y, 0.0);
+	EXPECT_DOUBLE_EQ(node->element.cylinder.orientation.z, 1.0);
+	EXPECT_DOUBLE_EQ(node->element.cylinder.diameter, 14.2);
+	EXPECT_DOUBLE_EQ(node->element.cylinder.height, 21.42);
+	EXPECT_EQ(node->element.cylinder.color, 0x0A00FF);
+
+	free(node);
+	ft_free_array(content);
+	
+	content = ft_split("50.0,0.0,20.6 14.2 21.42 10,0,255", ' ');
+	node = elist_new(cylinder, content);
 	EXPECT_EQ(node, nullptr);
 
 	free(node);
