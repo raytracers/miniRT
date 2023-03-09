b90490104f63b42aeb362ea26cef42d45743636d
lfarias- <lfarias-@student.42.rio>
2023-03-09 18:04:18 -0300

    test: Add more unit tests against wrong inputs in InputCylinder

--- UnitTests/SceneTests/InitCylinder.cpp
+++ UnitTests/SceneTests/InitCylinder.cpp
@@ -55,7 +55,7 @@ TEST(SceneTests, ValidCylinderTest)
 	ft_free_array(attributes);
 }
 
-/*void	test_init_cylinder(char *input_str)
+void	test_init_cylinder(char *input_str)
 {
 	union u_element element;
 	char			**attributes;	
@@ -67,23 +67,22 @@ TEST(SceneTests, ValidCylinderTest)
 	ft_free_array(attributes);
 }
 
-TEST(SceneTests, InvalidSphereTest)
+TEST(SceneTests, InvalidCylinderTest)
 {
 	union u_element element;
 	char			**attributes;	
-	int				op_result;	
 	
 	// missing fields
-	char *missing_color = (char *) "42.0,42.0,42.0 13.0";
+	char *missing_color = (char *) "42.0,42.0,42.0 13.0 12.0";
 	test_init_cylinder(missing_color);
 
-	char *missing_coords = (char *) "13.0 255,255,255";
+	char *missing_coords = (char *) "0.0,1.0,0.0 11.0 13.0 255,255,255";
 	test_init_cylinder(missing_coords);
 
-	char *missing_diameter = (char *) "0.0,0.0,0.0 255,255,255"; 
+	char *missing_diameter = (char *) "42.0,42.0,42.0 0.0,1.0,0.0 11.0 10.0 255,255,255"; 
 	test_init_cylinder(missing_diameter);
 
-	char *missing_all = (char *) " , , . , , "; 
+	char *missing_all = (char *) " , , , , . . , , "; 
 	test_init_cylinder(missing_all);
 
 	char *empty = (char *) ""; 
@@ -92,26 +91,26 @@ TEST(SceneTests, InvalidSphereTest)
 	char *empty2 = (char *) "                     "; 
 	test_init_cylinder(empty2);
 
-	char *no_spaces = (char *) "0.0,0.0,0.012.0255,0,255"; 
+	char *no_spaces = (char *) "0.0,0.0,0.00.0,0.0,11.0412.1312.0255,0,255"; 
 	test_init_cylinder(no_spaces);
 
-	char *wrong_delimiter = (char *) "0.0,0.0,0.0-12.0-255,0,255"; 
+	char *wrong_delimiter = (char *) "50.0,0.0,20.6-0.0,0.0,1.0-14.2-21.42-10,0,255";
 	test_init_cylinder(wrong_delimiter);
-
-	char *wrong_coord_delimiter = (char *) "0.0;0.0;0.0 12.0 255,0,255"; 
+ 
+	char *wrong_coord_delimiter = (char *) "50.0;0.0;20.6 0.0,0.0,1.0 14.2 21.42 10,0,255";
 	test_init_cylinder(wrong_coord_delimiter);
 
-	char *wrong_color_delimiter = (char *) "0.0,0.0,0.0 12.0 255;0;255"; 
+	char *wrong_color_delimiter = (char *) "50.0,0.0,20.6 0.0,0.0,1.0 14.2 21.42 10;0;255";
 	test_init_cylinder(wrong_color_delimiter);
 
 	// more fields than necessary
-	char *vec4_coord = (char *) "0.0,0.0,0.0,0.0 12.0 255,0,255"; 
+	char *vec4_coord = (char *) "0.0,0.0,0.0,0.0 1.0,0.0,0.0 12.0 11.0 255,0,255"; 
 	test_init_cylinder(vec4_coord);
 
-	char *double_diameter = (char *) "0.0,0.0,0.0 12.0 25.0 255,0,255"; 
-	test_init_cylinder(double_diameter);
+	char *triple_diameter = (char *) "0.0,0.0,0.0 1.0,0.0,0.0 12.0 25.0 30.0 255,0,255"; 
+	test_init_cylinder(triple_diameter);
 
-	char *cymk = (char *) "0.0,0.0,0.0 12.0 255,0,255,128"; 
+	char *cymk = (char *) "0.0,0.0,0.0 1.0,0.0,0.0 12.0 11.0 255,0,255,128"; 
 	test_init_cylinder(cymk);
-}*/
+}
 
