7517971df9a9ba6b7ae862c0698a379622138e26
lfarias- <lfarias-@student.42.rio>
2023-03-09 19:57:35 -0300

    Fix: fix init_plane.c verifications and add more tests

--- UnitTests/SceneTests/InitPlane.cpp
+++ UnitTests/SceneTests/InitPlane.cpp
@@ -46,3 +46,74 @@ TEST(SceneTests, InitPlaneTest)
 	EXPECT_EQ(element.plane.color, 0x000000);
 	ft_free_array(attributes);
 }
+
+void	test_init_plane(char *input_str)
+{
+	union u_element element;
+	char			**attributes;	
+	int				op_result;	
+
+	attributes = ft_split(input_str, ' ');
+	op_result = init_plane(attributes, &element);
+	EXPECT_EQ(op_result, 1);
+	ft_free_array(attributes);
+}
+
+TEST(SceneTests, MissingArgPlaneTest)
+{
+	union u_element element;
+	char			**attributes;	
+	int				op_result;	
+	
+	// missing fields
+	char *missing_color = (char *) "0.0,0.0,-10.0 0.0,1.0,0.0";
+	test_init_plane(missing_color);
+
+	char *missing_coords = (char *) "0.0,1.0,0.0 0,0,225";
+	test_init_plane(missing_coords);
+
+	char *missing_normal = (char *) "0.0,0.0,-10.0 255,255,255"; 
+	test_init_plane(missing_normal);
+
+	char *normalized = (char *) "0.0,0.0,-10.0 0.0,0.0,0.0 255,255,255";
+	test_init_plane(normalized);
+
+}
+
+TEST(SceneTests, WrongFormatPlaneTest)
+{
+	union u_element element;
+	char			**attributes;	
+	int				op_result;	
+
+	char *missing_all = (char *) " , , . , , "; 
+	test_init_plane(missing_all);
+
+	char *empty = (char *) ""; 
+	test_init_plane(empty);
+
+	char *empty2 = (char *) "                     "; 
+	test_init_plane(empty2);
+
+	char *no_spaces = (char *) "0.0,0.0,-10.00.0,0.0,0.055,0,255"; 
+	test_init_plane(no_spaces);
+
+	char *wrong_delimiter = (char *) "0.0,0.0,0.0-0.0,1.0,0.0-255,0,255"; 
+	test_init_plane(wrong_delimiter);
+
+	char *wrong_coord_delimiter = (char *) "0.0;0.0;0.0 0.0;1.0;0.0 255,0,255"; 
+	test_init_plane(wrong_coord_delimiter);
+
+	char *wrong_color_delimiter = (char *) "0.0,0.0,0.0 0.0,1.0,0.0 255;0;255"; 
+	test_init_plane(wrong_color_delimiter);
+
+	// more fields than necessary
+	char *vec4_coord = (char *) "0.0,0.0,0.0,0.0 0.0,1.0,0.0 255,0,255"; 
+	test_init_plane(vec4_coord);
+
+	char *double_normal = (char *) "0.0,0.0,0.0 0.0,1.0,0.0 0.0,1.0,0.0 255,0,255"; 
+	test_init_plane(double_normal);
+
+	char *cymk = (char *) "0.0,0.0,0.0 0.0,1.0,0.0 255,0,255,128"; 
+	test_init_plane(cymk);
+}
--- sources/input/init_plane.c
+++ sources/input/init_plane.c
@@ -6,7 +6,7 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 18:15:42 by lfarias-          #+#    #+#             */
-/*   Updated: 2023/03/09 18:46:11 by lfarias-         ###   ########.fr       */
+/*   Updated: 2023/03/09 19:54:57 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,16 @@ int	init_plane(char **attributes, union u_element *element)
 	int			color;
 	t_plane		*plane;
 
-	color = 0;
+	color = -1;
 	attr_len = ft_arraylen(attributes);
 	plane = &element->plane;
 	if (attr_len != 3)
 		return (1);
 	if (get_point(attributes[0], &plane->origin))
 		return (1);
-	if (get_point(attributes[1], &plane->normal) && \
-		is_normalized(plane->normal))
+	if (get_point(attributes[1], &plane->normal))
+		return (1);
+	if (!is_normalized(plane->normal))
 		return (1);
 	color = get_color(attributes[2]);
 	if (color == -1)
