d82677b608c13c8651c707aff00c9b4c775c4f51
lfarias- (Leonardo Farias) <leofariasrj25@gmail.com>
2023-03-10 20:48:22 -0300

    feat: init_light implementation and two more basic tests

--- UnitTests/SceneTests/InitLight.cpp
+++ UnitTests/SceneTests/InitLight.cpp
@@ -10,13 +10,31 @@ TEST(SceneTests, ValidLight)
 	char		**attributes;
 	t_light		light;
 
-{
-	attributes = ft_split((char *) "-40.0,50.0,0.0 0.6", ' ');
-	EXPECT_EQ(init_light(attributes, &light), 0);
-	EXPECT_DOUBLE_EQ(light.origin.x, -40.0);
-	EXPECT_DOUBLE_EQ(light.origin.y, 50.0);
-	EXPECT_DOUBLE_EQ(light.origin.y, 0.0);
-	EXPECT_DOUBLE_EQ(light.brightness, 0.6);
-}
-	
+	{
+		attributes = ft_split((char *) "-40.0,50.0,0.0 0.6", ' ');
+		EXPECT_EQ(init_light(attributes, &light), 0);
+		EXPECT_DOUBLE_EQ(light.origin.x, -40.0);
+		EXPECT_DOUBLE_EQ(light.origin.y, 50.0);
+		EXPECT_DOUBLE_EQ(light.origin.z, 0.0);
+		EXPECT_DOUBLE_EQ(light.brightness, 0.6);
+		ft_free_array(attributes);
+	}
+	{
+		attributes = ft_split((char *) "0.0,0.0,0.0 0.0", ' ');
+		EXPECT_EQ(init_light(attributes, &light), 0);
+		EXPECT_DOUBLE_EQ(light.origin.x, 0.0);
+		EXPECT_DOUBLE_EQ(light.origin.y, 0.0);
+		EXPECT_DOUBLE_EQ(light.origin.z, 0.0);
+		EXPECT_DOUBLE_EQ(light.brightness, 0.0);
+		ft_free_array(attributes);
+	}
+	{
+		attributes = ft_split((char *) "42.0,42.0,42.0 1.0", ' ');
+		EXPECT_EQ(init_light(attributes, &light), 0);
+		EXPECT_DOUBLE_EQ(light.origin.x, 42.0);
+		EXPECT_DOUBLE_EQ(light.origin.y, 42.0);
+		EXPECT_DOUBLE_EQ(light.origin.z, 42.0);
+		EXPECT_DOUBLE_EQ(light.brightness, 1.0);
+		ft_free_array(attributes);
+	}
 }
--- sources/types/init_light.c
+++ sources/types/init_light.c
@@ -6,7 +6,7 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 20:12:54 by lfarias-          #+#    #+#             */
-/*   Updated: 2023/03/10 20:18:45 by lfarias-         ###   ########.fr       */
+/*   Updated: 2023/03/10 20:46:41 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,16 @@
 
 int	init_light(char **attributes, t_light *light)
 {
-	(void)attributes;
-	(void)light;
-	return (1);
+	double brightness;
+	if (ft_arraylen(attributes) != 2)
+		return (1);
+	if (get_point(attributes[0], &light->origin))
+		return (1); 
+	if (!ft_isdouble_str(attributes[1]))
+		return (1);
+	brightness = ft_atof(attributes[1]);
+	if (!(brightness >= 0.0 && brightness <= 1.0))
+		return (1);
+	light->brightness = brightness;
+	return (0);
 }
