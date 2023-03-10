f9de0242e0b36b2a76736f2ab45ff3ade085dd1d
lfarias- (Leonardo Farias) <leofariasrj25@gmail.com>
2023-03-10 19:14:09 -0300

    feat: Add init_ambient_light implementation and some tests

--- UnitTests/SceneTests/InitAmbientLight.cpp
+++ UnitTests/SceneTests/InitAmbientLight.cpp
@@ -14,4 +14,17 @@ TEST(SceneTests, ValidAmbientLight)
 	EXPECT_EQ(init_ambient_light(attributes, &amb_light), 0);
 	EXPECT_DOUBLE_EQ(amb_light.ratio, 0.2);
 	EXPECT_EQ(amb_light.color, 0xFFFFFF);
+	ft_free_array(attributes);
+
+	attributes = ft_split((char *) "0.0 0,0,0", ' ');
+	EXPECT_EQ(init_ambient_light(attributes, &amb_light), 0);
+	EXPECT_DOUBLE_EQ(amb_light.ratio, 0.0);
+	EXPECT_EQ(amb_light.color, 0x000000);
+	ft_free_array(attributes);
+
+	attributes = ft_split((char *) "2.0 255,255,255", ' ');
+	EXPECT_EQ(init_ambient_light(attributes, &amb_light), 0);
+	EXPECT_DOUBLE_EQ(amb_light.ratio, 2.0);
+	EXPECT_EQ(amb_light.color, 0xFFFFFF);
+	ft_free_array(attributes);
 }
--- sources/types/init_ambient_light.c
+++ sources/types/init_ambient_light.c
@@ -6,7 +6,7 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 14:04:48 by lfarias-          #+#    #+#             */
-/*   Updated: 2023/03/10 14:13:27 by lfarias-         ###   ########.fr       */
+/*   Updated: 2023/03/10 19:08:21 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,22 @@
 
 int	init_ambient_light(char **attributes, t_a_light *amb_light)
 {
-	(void)attributes;
-	(void)amb_light;
-	return (1);
+	double	ratio;
+	int		color;
+
+	if (ft_arraylen(attributes) != 2)
+		return (1);
+	if (!ft_isdouble_str(attributes[0]))
+		return (1);
+	ratio = ft_atof(attributes[0]);	
+	if (!(ratio >= 0.0 && ratio <= 2.0))
+		return (1);
+	color = get_color(attributes[1]);
+	if (color == -1)
+		return (1);
+	amb_light->ratio = ratio;
+	amb_light->color = color;
+	return (0);
 }
 
 
