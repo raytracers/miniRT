cca0d58a0d48de030ce152c9fa8548a4fd3beb12
GabrielCLC <gabrielclcardoso@gmail.com>
2023-03-10 12:22:45 -0300

    HotFix: Fixed a if condition inside the init_cylinder function

--- sources/types/init_cylinder.c
+++ sources/types/init_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 15:32:20 by lfarias-          #+#    #+#             */
-/*   Updated: 2023/03/10 11:52:17 by gcorreia         ###   ########.fr       */
+/*   Updated: 2023/03/10 12:21:52 by gcorreia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,16 @@
 
 int	init_cylinder(char **attributes, union u_object *object)
 {
-	int			attr_len;
 	int			color;
 	t_cylinder	*cylinder;
 
-	color = 0;
-	attr_len = ft_arraylen(attributes);
 	cylinder = &object->cylinder;
-	if (attr_len != 5)
+	if (ft_arraylen(attributes)!= 5)
 		return (1);
 	if (get_point(attributes[0], &cylinder->origin))
 		return (1);
-	if (get_point(attributes[1], &cylinder->orientation) && \
-		is_normalized(cylinder->orientation))
+	if (get_point(attributes[1], &cylinder->orientation) || \
+		!is_normalized(cylinder->orientation))
 		return (1);
 	if (!ft_isdouble_str(attributes[2]) || !ft_isdouble_str(attributes[3]))
 		return (1);
