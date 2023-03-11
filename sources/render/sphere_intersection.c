fda2e7dbdd5c2b7bc525e4689b67ed93183ab067
GabrielCLC <gabrielclcardoso@gmail.com>
2023-03-10 21:08:25 -0300

    Feat: prototyped function

---
+++ sources/render/sphere_intersection.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sphere_intersection.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcorreia <gcorreia@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/10 17:59:37 by gcorreia          #+#    #+#             */
+/*   Updated: 2023/03/10 18:02:49 by gcorreia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/mini_rt.h"
+
+t_intersection	sphere_intersection(t_ray ray, union u_object *object)
+{
+	t_intersection	point;
+	t_sphere		*sphere;
+
+	(void)point;
+	(void)sphere;
+	(void)ray;
+	(void)object;
+	return (point);
+}
