129e9ff9157f51f30a55c9088614421ba2afbe8e
lfarias- (Leonardo Farias) <leofariasrj25@gmail.com>
2023-03-10 21:07:54 -0300

    Test: Add more tests to InitLight.cpp, all ok

--- UnitTests/SceneTests/InitLight.cpp
+++ UnitTests/SceneTests/InitLight.cpp
@@ -38,3 +38,73 @@ TEST(SceneTests, ValidLight)
 		ft_free_array(attributes);
 	}
 }
+
+void	test_init_light(char *str)
+{
+	char		**attributes;
+	t_light		light;
+
+	attributes = ft_split(str, ' ');
+	EXPECT_EQ(init_light(attributes, &light), 1);
+	ft_free_array(attributes);
+}
+
+TEST(SceneTests, MissingArgInitLight)
+{
+	char *missing_brightness = (char *) "-40.0,50.0,0.0";
+	test_init_light(missing_brightness);
+
+	char *missing_origin = (char *) "1.0";
+	test_init_light(missing_origin);
+
+	char *no_arg = (char *) "";
+	test_init_light(no_arg);
+
+	char *empty = (char *) "                              ";
+	test_init_light(empty);
+}
+
+TEST(SceneTests, ExcessArgInitLight)
+{
+	char *excess_brightness = (char *) "-40.0,50.0,0.0 0.5 0.6";
+	test_init_light(excess_brightness);
+
+	char *excess_point_field = (char *) "-40.0,50.0,0.0,1.0 0.5";
+	test_init_light(excess_point_field);
+}
+
+TEST(SceneTests, InvalidArgInitLight)
+{
+	char *out_of_range_brightness = (char *) "-40.0,50.0,0.0 1.1";
+	test_init_light(out_of_range_brightness);
+
+	char *out_of_range_brightness2 = (char *) "-40.0,50.0,0.0 -0.1";
+	test_init_light(out_of_range_brightness2);
+
+	char *out_of_range_brightness3 = (char *) "-40.0,50.0,0.0 -1.0";
+	test_init_light(out_of_range_brightness3);
+
+	char *wrong_order = (char *) "0.2 -40.0,50.0,0.0";
+	test_init_light(wrong_order);
+}
+
+TEST(SceneTests, WrongFormatArgInitLight)
+{
+	char *wrong_brightness_delim = (char *) "-40.0,50.0,0.0 0,2";
+	test_init_light(wrong_brightness_delim);
+
+	char *wrong_point_delim = (char *) "-40.0;50.0;0.0 0.2";
+	test_init_light(wrong_point_delim);
+
+	char *just_delim = (char *) " . , , ";
+	test_init_light(just_delim);
+
+	char *no_space = (char *) "-40.0,50.0,0.00.2";
+	test_init_light(no_space);
+ 
+	char *wrong_separator = (char *) "-40.0,50.0,0.0-0.2";
+	test_init_light(wrong_separator);
+
+	char *missing_point_component = (char *) "-40.0,0.0 0.2";
+	test_init_light(missing_point_component);
+}
