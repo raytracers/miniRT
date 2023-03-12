aacd5eb4356c50bf00be1137ef0b24818b058ecf
lfarias- (Leonardo Farias) <leofariasrj25@gmail.com>
2023-03-12 18:49:54 -0300

    refactor: move read line logic and line treatment to its own function

--- sources/input/scene_loader.c
+++ sources/input/scene_loader.c
@@ -6,42 +6,39 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 22:00:57 by lfarias-          #+#    #+#             */
-/*   Updated: 2023/03/12 01:57:27 by lfarias-         ###   ########.fr       */
+/*   Updated: 2023/03/12 18:48:53 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/mini_rt.h"
+#include <stdio.h>
 
-void	free_resources(char *line , char **line_fields);
-int		elem_exists(enum e_element e_type, t_scene *scene);
+#define SCENE_EOF -1
+#define SCENE_EMPTY_LN 1 
+
+void	free_resources(char *line, char **line_fields);
+int		read_line(int scene_fd, char **line);
 int		get_uniq_elem(enum e_element e_type, char **line_f, t_scene *scene);
 int		get_primitive(enum e_element e_type, char **line_f, t_scene *scene);
-
-#include <stdio.h>
 
 int	scene_load(int scene_fd, t_scene *scene)
 {
 	enum e_element	e_type;
+	int				op_code;
 	char			*line;
 	char			**line_fields;
 
 	line = NULL;
 	while (42)
 	{
-		line = get_next_line(scene_fd);
-		if (!line)
+		op_code = read_line(scene_fd, &line);
+		if (op_code == SCENE_EOF)
 			break ;
-		if (*line == '\n')
-		{
-			free(line);
+		if (op_code == SCENE_EMPTY_LN)
 			continue ;
-		}
-		int line_size = ft_strlen(line);
-		line[line_size - 1] = '\0';
 		line_fields = ft_split(line, ' ');
-		e_type = get_element(line_fields[0]);	
-		if (e_type == nae \
-			|| get_uniq_elem(e_type, &line_fields[1], scene) \
+		e_type = get_element(line_fields[0]);
+		if (e_type == nae || get_uniq_elem(e_type, &line_fields[1], scene) \
 			|| get_primitive(e_type, &line_fields[1], scene))
 		{
 			free_resources(line, line_fields);
@@ -49,16 +46,29 @@ int	scene_load(int scene_fd, t_scene *scene)
 		}
 		free_resources(line, line_fields);
 	}
-	t_elist *node = scene->elements;
-	while (node)
-	{
-		printf("type = %d\n", node->type);
-		node = node->next;
-	}
 	return (0);
 }
 
-void	free_resources(char *line , char **line_fields)
+int	read_line(int scene_fd, char **line)
+{
+	int	line_size;
+
+	line_size = 0;
+	*line = get_next_line(scene_fd);
+	if (!(*line))
+		return (SCENE_EOF);
+	if (**line == '\n')
+	{
+		free(*line);
+		return (SCENE_EMPTY_LN);
+	}
+	line_size = ft_strlen(*line);
+	if (line_size > 0 && line[0][line_size - 1] == '\n')
+		line[0][line_size - 1] = '\0';
+	return (0);
+}
+
+void	free_resources(char *line, char **line_fields)
 {
 	free(line);
 	ft_free_array(line_fields);
@@ -66,7 +76,11 @@ void	free_resources(char *line , char **line_fields)
 
 int	get_uniq_elem(enum e_element e_type, char **line_f, t_scene *scene)
 {
-	if (elem_exists(e_type, scene))
+	if (e_type == camera && scene->camera != NULL)
+		return (1);
+	if (e_type == light && scene->light != NULL)
+		return (1);
+	if (e_type == ambient_light && scene->a_light != NULL)
 		return (1);
 	if (e_type == camera)
 	{
@@ -88,22 +102,14 @@ int	get_uniq_elem(enum e_element e_type, char **line_f, t_scene *scene)
 	return (0);
 }
 
-int	elem_exists(enum e_element e_type, t_scene *scene)
+int	get_primitive(enum e_element e_type, char **line_f, t_scene *scene)
 {
-	if (e_type == camera && scene->camera != NULL)
-		return (1);
-	if (e_type == light && scene->light != NULL)
-		return (1);
-	if (e_type == ambient_light && scene->a_light != NULL)
-		return (1);
-	return (0);
-}
+	t_elist	*new_elem;
 
-int get_primitive(enum e_element e_type, char **line_f, t_scene *scene)
-{
+	new_elem = NULL;
 	if (e_type != ambient_light && e_type != camera && e_type != light)
 	{
-		t_elist *new_elem = elist_new(e_type, line_f);
+		new_elem = elist_new(e_type, line_f);
 		if (!new_elem)
 			return (1);
 		elist_addback(&scene->elements, new_elem);
