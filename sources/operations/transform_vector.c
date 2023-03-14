bebf74136a3f2ca7c71d8743ea3274348eb178f4
Gabriel Correia lima cardoso <gcorreia@c1t3s4.42.rio>
2023-03-14 16:07:01 -0300

    Feat: Implemented transform_vector function

--- sources/operations/transform_vector.c
+++ sources/operations/transform_vector.c
@@ -6,7 +6,7 @@
 /*   By: gcorreia <gcorreia@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 15:17:31 by gcorreia          #+#    #+#             */
-/*   Updated: 2023/03/14 15:19:22 by gcorreia         ###   ########.fr       */
+/*   Updated: 2023/03/14 16:06:16 by gcorreia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 t_point	transform_vector(t_point v, double **m)
 {
-	(void)m;
-	return (v);
+	t_point	r;
+
+	r.x = m[0][0] * v.x + m[0][1] * v.y + m[0][2] * v.z + m[0][3];
+	r.y = m[1][0] * v.x + m[1][1] * v.y + m[1][2] * v.z + m[1][3];
+	r.z = m[2][0] * v.x + m[2][1] * v.y + m[2][2] * v.z + m[2][3];
+	return (r);
 }
