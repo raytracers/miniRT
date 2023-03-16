6a9c53b0a0087c166a75b02a3f342e4f1663858c
Gabriel Correia lima cardoso <gcorreia@c1t2s7.42.rio>
2023-03-16 13:54:14 -0300

    Fix: changed argument from transform vector to double**

--- sources/operations/transform_vector.c
+++ sources/operations/transform_vector.c
@@ -6,13 +6,13 @@
 /*   By: gcorreia <gcorreia@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 15:17:31 by gcorreia          #+#    #+#             */
-/*   Updated: 2023/03/14 16:06:16 by gcorreia         ###   ########.fr       */
+/*   Updated: 2023/03/14 17:49:06 by gcorreia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/types.h"
 
-t_point	transform_vector(t_point v, double **m)
+t_point	transform_vector(t_point v, double *m[4])
 {
 	t_point	r;
 
