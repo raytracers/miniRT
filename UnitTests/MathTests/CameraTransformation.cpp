f2fd0e8f0ac83a612129e29d942b4373de8269e7
GabrielCLC <gabrielclcardoso@gmail.com>
2023-03-17 20:28:17 -0300

    Fix: Fixed Camera transformation tests

--- UnitTests/MathTests/CameraTransformation.cpp
+++ UnitTests/MathTests/CameraTransformation.cpp
@@ -1,23 +1,39 @@
 #include <gtest/gtest.h>
+#include <iostream>
 
 extern "C"
 {
 	#include "../../headers/mini_rt.h"
 }
 
-TEST(CameraTransformation, CameraAtRandompos)
+void	test_rotation(char *str)
 {
 	t_camera	*c;
-	char		**attr = ft_split("-50.0,0,20 1,0,0 70", ' ');
+	double		**vtm;
+	double		**rtm;
+	char		**attr;
+
+	attr = ft_split(str, ' ');
 	init_camera(attr, &c);
 	ASSERT_NE(c, nullptr);
-	double	**vtm = get_vtm(c);
-	transform_camera(c, vtm);
+	vtm = get_vtm(c);
+	ASSERT_NE(vtm, nullptr);
+	rtm = get_rtm(c);
+	ASSERT_NE(rtm, nullptr);
+	transform_camera(c, vtm, rtm);
 
 	EXPECT_DOUBLE_EQ(c->origin.x, 0);
 	EXPECT_DOUBLE_EQ(c->origin.y, 0);
 	EXPECT_DOUBLE_EQ(c->origin.z, 0);
-	EXPECT_DOUBLE_EQ(c->orientation.x, 0);
-	EXPECT_DOUBLE_EQ(c->orientation.y, 0);
-	EXPECT_DOUBLE_EQ(c->orientation.z, 0);
+	EXPECT_TRUE(c->orientation.x > -0.001 && c->orientation.x < 0.001);
+	EXPECT_TRUE(c->orientation.y > -0.001 && c->orientation.y < 0.001);
+	EXPECT_DOUBLE_EQ(c->orientation.z, -1);
+}
+
+TEST(CameraTransformation, CameraAtRandompos)
+{
+	test_rotation((char *)"-50.0,0,20 0.424,0.566,0.707 70");
+	test_rotation((char *)"-50.0,0,20 0,0,1 70");
+	test_rotation((char *)"-50.0,0,20 0,0.707,0.707 70");
+	test_rotation((char *)"-50.0,0,20 0,-0.707,0.707 70");
 }
