4f33d4bdd5095c2a216083847bbb40ac947ede16
GabrielCLC <gabrielclcardoso@gmail.com>
2023-03-20 11:41:14 -0300

    Refactor: refactored CameraTransformation.cpp into RotationMatrix.cpp

--- UnitTests/MathTests/RotationMatrix.cpp
+++ UnitTests/MathTests/RotationMatrix.cpp
@@ -17,26 +17,23 @@ void	test_rotation(const char *str)
 	ASSERT_NE(c, nullptr);
 	rtm = get_rtm(c);
 	ASSERT_NE(rtm, nullptr);
-	vtm = get_vtm(rtm, c);
-	ASSERT_NE(vtm, nullptr);
-	transform_camera(c, vtm, rtm);
+	c->orientation = transform_vector(c->orientation, rtm);
 
-	EXPECT_DOUBLE_EQ(c->origin.x, 0);
-	EXPECT_DOUBLE_EQ(c->origin.y, 0);
-	EXPECT_DOUBLE_EQ(c->origin.z, 0);
 	EXPECT_TRUE(c->orientation.x > -0.001 && c->orientation.x < 0.001);
 	EXPECT_TRUE(c->orientation.y > -0.001 && c->orientation.y < 0.001);
 	EXPECT_DOUBLE_EQ(c->orientation.z, -1);
 }
 
-TEST(CameraTransformation, CameraAtRandompos)
+TEST(RotationMatrix, VectorAtRandomOrientation)
 {
 	test_rotation("-50.0,0,20 0.424,0.566,0.707 70");
 	test_rotation("42,21,84 0,0.707,0.707 70");
 	test_rotation("-50,-50,-50 0,-0.707,0.707 70");
+	test_rotation("-50,-50,-50 0,1,0 70");
+	test_rotation("-50,-50,-50 1,0,0 70");
 }
 
-TEST(CameraTransformation, CameraAlignedWithZ)
+TEST(RotationMatrix, VectorAlignedWithZaxis)
 {
 	test_rotation("0,0,0 0,0,1 70");
 	test_rotation("42,42,42 0,0,-1 70");
