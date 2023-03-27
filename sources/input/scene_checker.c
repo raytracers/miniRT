bbf108a873cfae58d37b3a5727f98f4efab0a05f
lfarias- <lfarias-@student.42.rio>
2023-03-27 22:56:03 +0000

    Test: Add tests for scene checker

--- sources/input/scene_checker.c
+++ sources/input/scene_checker.c
@@ -6,7 +6,7 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 18:07:32 by lfarias-          #+#    #+#             */
-/*   Updated: 2023/03/27 21:31:44 by lfarias-         ###   ########.fr       */
+/*   Updated: 2023/03/27 22:49:31 by freemanc1        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	scene_check(t_scene *scene)
 {
 	if (scene->camera == NULL)
 	{
-		ft_putstr_fd("ERROR: No camera data found\n", STDERR_FILENO);		
+		ft_putstr_fd("ERROR: No camera data found\n", STDERR_FILENO);
 		return (1);
 	}
 	if (scene->light == NULL)
