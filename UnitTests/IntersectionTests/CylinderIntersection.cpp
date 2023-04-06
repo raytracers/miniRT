d0256bd23400896f6752daa041ca7c4da6e90879
lfarias- <lfarias-@student.42.rio>
2023-04-06 20:48:53 -0300

    Test: Add more tests

---
+++ UnitTests/IntersectionTests/CylinderIntersection.cpp
@@ -0,0 +1,65 @@
+#include <gtest/gtest.h>
+
+extern "C"
+{
+	#include "../../headers/mini_rt.h"
+}
+
+void init_scene(t_scene *scene);
+
+TEST(PlaneIntersection, SimpleCylinderIntersect)
+{
+	u_object		cy;
+	t_intersection	inter;
+
+{
+	char **attributes = ft_split("0,10,0 0.0,1.0,0.0 12.4 10.42 10,0,255", ' ');
+	ASSERT_FALSE(init_cylinder(attributes, &cy));
+	t_point origin = { 0, 0, 0};
+	t_point direction = {0, 1, 0 };
+	t_ray ray = {origin, direction};
+	inter = cylinder_intersection(ray, cy);
+}
+	EXPECT_TRUE(inter.exists);
+	EXPECT_DOUBLE_EQ(inter.distance, 10);
+	EXPECT_DOUBLE_EQ(inter.location.x, 0);
+	EXPECT_DOUBLE_EQ(inter.location.y, 10);
+	EXPECT_DOUBLE_EQ(inter.location.z, 0);
+	EXPECT_DOUBLE_EQ(inter.normal.x, 0);
+	EXPECT_DOUBLE_EQ(inter.normal.y, -1);
+	EXPECT_DOUBLE_EQ(inter.normal.z, 0);
+	EXPECT_DOUBLE_EQ(inter.color, 655615);
+{
+	// aligned on the x axis.
+	char **attributes = ft_split("10,10,0 -1.0,0.0,0.0 12.4 10.42 10,0,255", ' ');
+	ASSERT_FALSE(init_cylinder(attributes, &cy));
+	t_point origin = { 0, 0, 0};
+	t_point direction = {0, 1, 0 };
+	t_ray ray = {origin, direction};
+	inter = cylinder_intersection(ray, cy);
+}
+	EXPECT_TRUE(inter.exists);
+	EXPECT_DOUBLE_EQ(inter.distance, 3.8);
+	EXPECT_DOUBLE_EQ(inter.location.x, 0);
+	EXPECT_DOUBLE_EQ(inter.location.y, 3.8);
+	EXPECT_DOUBLE_EQ(inter.location.z, 0);
+	EXPECT_DOUBLE_EQ(inter.normal.x, 0);
+	EXPECT_DOUBLE_EQ(inter.normal.y, -1);
+	EXPECT_DOUBLE_EQ(inter.normal.z, 0);
+	EXPECT_DOUBLE_EQ(inter.color, 655615);
+}
+
+TEST(SphereIntersection, NoIntersectionCylinder)
+{
+	char **attributes = ft_split("0,5,0 1.0,0.0,0.0 12.4 10.42 10,0,255", ' ');
+	u_object		cy;
+	t_intersection	inter;
+
+	ASSERT_FALSE(init_cylinder(attributes, &cy));
+	t_point origin = { 0, 0, 0};
+	t_point direction = {0, -1, 0 };
+	t_ray ray = {origin, direction};
+	inter = cylinder_intersection(ray, cy);
+
+	ASSERT_FALSE(inter.exists);
+}
--- headers/operations.h
+++ headers/operations.h
@@ -6,7 +6,7 @@
 /*   By: gcorreia <gcorreia@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 12:49:59 by gcorreia          #+#    #+#             */
-/*   Updated: 2023/04/05 02:50:29 by lfarias-         ###   ########.fr       */
+/*   Updated: 2023/04/06 16:49:45 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 /* ************************* ALGEBRIC OPERATIONS **************************** */
 
-double	quadratic_solver(double a, double b, double c);
+double	quadratic_solver(double b, double c);
 int		cylinder_quadratic(t_ray ray, t_cylinder cy, double *t1, double *t2);
 
 /* ************************************************************************** */
