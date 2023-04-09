5543e5edd476df415e2d5bcf8e6f88d93b8950f8
Gabriel Cardoso <69059969+gabrielclcardoso@users.noreply.github.com>
2023-04-09 13:40:47 -0300

    Fix: intersection when camera is inside shere now behaves correctly (#88)
    hotfix, merged it myself.

--- sources/operations/quadratic_solver.c
+++ sources/operations/quadratic_solver.c
@@ -6,13 +6,13 @@
 /*   By: gcorreia <gcorreia@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 11:39:41 by gcorreia          #+#    #+#             */
-/*   Updated: 2023/03/11 12:26:42 by gcorreia         ###   ########.fr       */
+/*   Updated: 2023/04/06 10:22:51 by gcorreia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/mini_rt.h"
 
-double	quadratic_solver(double b, double c)
+double	quadratic_solver(double b, double c, int *flag)
 {
 	double	delta;
 	double	sol_one;
@@ -25,6 +25,8 @@ double	quadratic_solver(double b, double c)
 	delta = sqrt(delta);
 	sol_one = (b + delta) / 2;
 	sol_two = (b - delta) / 2;
+	if ((sol_one < 0 && sol_two >= 0) || (sol_one >= 0 && sol_two < 0))
+		*flag = 1;
 	if (sol_one < 0 && sol_two < 0)
 		return (-1);
 	else if (sol_one > 0 && sol_two < 0)
